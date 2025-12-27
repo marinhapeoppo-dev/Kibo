0f8e6ac1e4e5d6a887936a3768e08192d438f622
Indraprasta <marinhapeoppo@gmail.com>
2025-12-28 02:53:32 +0700

    Change cheerio import to namespace import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import fetch from 'node-fetch';
 import path from 'path';
 import { fileURLToPath } from 'url';
